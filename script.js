ea19fa05ae55d2e05166d1ac4cccaccd42bc3859
WitchingHr <matt1494@gmail.com>
2022-11-09 23:10:51 -0500

    Add password mismatch error message

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ submit.addEventListener('click', () => {
         item.classList.add('invalid');
     });
 });
+
+function checkMatch() {
+    const password = document.getElementById('password').value;
+    const confirm = document.getElementById('confirm').value;
+    if (confirm !== password) {
+        document.querySelector('button').disabled = true;
+        document.querySelector('.password_error').style.opacity = 1
+    } else {
+        document.querySelector('button').disabled = false;
+        document.querySelector('.password_error').style.opacity = 0
+    }
+}
+
