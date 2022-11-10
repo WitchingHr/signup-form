50febcb85dec048945c78b8185b01dfb837294a9
WitchingHr <matt1494@gmail.com>
2022-11-10 11:39:16 -0500

    Add remove animation function to error message

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function checkMatch() {
     const password = document.getElementById('password').value;
     const confirm = document.getElementById('confirm').value;
     if (confirm !== password) {
-        document.querySelector('button').disabled = true;
+        // document.querySelector('button').disabled = true;
         document.querySelector('.password_error').style.opacity = 1
     } else {
-        document.querySelector('button').disabled = false;
+        // document.querySelector('button').disabled = false;
         document.querySelector('.password_error').style.opacity = 0
     }
 }
@@ -21,4 +21,9 @@ function checkMatch() {
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     document.querySelector('.password_error').classList.add('shake');
+})
+
+const error = document.querySelector('.password_error');
+error.addEventListener('animationend', () => {
+    error.classList.remove('shake');
 })
